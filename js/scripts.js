4b7f73f724fc8cafe5672a3fcb46aaf6fa8a8ab4
Ross Fletcher and Zach Evans <ross@>
2017-12-12 15:50:06 -0800

    Add files and basic functionality.

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+//back end
+var total;
+
+function Customer(name, initialDeposit) {
+  // debugger;
+
+  this.customerName = name;
+  this.initialAmount = initialDeposit;
+
+  if (isNaN(initialDeposit)) {
+    total = 0;
+  } else {
+    total = initialDeposit;
+  }
+};
+
+Customer.prototype.transaction = function(deposit, withdrawal) {
+  debugger;
+  if (!isNaN(deposit)) {
+    total += deposit;
+  } else {
+
+  }
+
+  if (!isNaN(withdrawal)) {
+    total -= withdrawal;
+  } else {
+
+  }
+};
+
+//front end
+$(document).ready(function() {
+  $("#open-acc").submit(function(event) {
+    event.preventDefault();
+    var inputtedName = $("#user-name").val();
+    var inputInitialDeposit = parseFloat($("#initial-deposit").val());
+    user = new Customer(inputtedName, inputInitialDeposit);
+
+    $("#user-name").val("");
+    $("#initial-deposit").val("");
+  });
+
+  $("#transaction").submit(function(event) {
+    event.preventDefault();
+    var deposit = parseFloat($("#user-deposit").val());
+    var withdrawal = parseFloat($("#user-withdrawal").val());
+    user.transaction(deposit, withdrawal);
+    $("#user-deposit").val("");
+    $("#user-withdrawal").val("");
+  })
+
+
+});
